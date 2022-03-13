c60a580ecfeb782aafb63a04e720f7718ed6c54a
Vladimir-Zel <vladimir.zelenov00@gmail.com>
2022-03-13 19:52:19 +0200

    les

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {v4 as uuidv4} from 'uuid';
+import {randomColor} from 'randomcolor'
+import Draggable from "react-draggable";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [item, setItem] = useState('')
+    const [storageItems, setStorageItems] = useState(JSON.parse(localStorage.getItem('storageItems')) || [])
+
+    useEffect(() => {
+        localStorage.setItem('storageItems', JSON.stringify(storageItems))
+    }, [storageItems]);
+
+    const newItem = () => {
+        if (item.trim() !== '') {
+            const newItem = {
+                id: uuidv4(),
+                item,
+                color: randomColor({
+                    luminosity: 'light'
+                }),
+                defaultPosition: {
+                    x: 500,
+                    y: -500
+                }
+            }
+            setStorageItems((storageItems) => [...storageItems, newItem])
+            setItem('')
+        } else {
+            alert("Enter something...")
+            setItem('')
+        }
+    }
+
+    const deleteNode = (id)  => {
+      setStorageItems(storageItems.filter((item) => item.id !== id))
+    }
+
+    const updatePos = (data, index) => {
+        let objects = [...storageItems]
+        objects[index].defaultPosition ={x: data.x, y: data.y}
+        setStorageItems(objects)
+    }
+
+    const keyPress = (e) => {
+        const code = e.which || e.keyCode
+        if (code === 13) {
+            newItem()
+        }
+    }
+
+    return (
+        <div className="App">
+            <div className="wrapper">
+                <input value={item}
+                       onChange={(e) => setItem(e.target.value)}
+                       onKeyPress={(e) => keyPress(e)}
+                       type='text'
+                       placeholder='enter something...'
+
+                />
+                <button className='enter' onClick={() => newItem()}>ENTER</button>
+            </div>
+
+            {storageItems.map((item, index) => {
+                return (
+                    <Draggable
+                        key={index} defaultPosition={item.defaultPosition}
+                        onStop={(_,data) => {
+                    updatePos(data, index)
+                }
+                        }>
+                        <div className='card' style={{backgroundColor: item.color}}>
+                            {`${item.item}`}
+                            <button className='delete' onClick={() => deleteNode(item.id)}> x</button>
+                        </div>
+                    </Draggable>
+                )
+            })}
+        </div>
+    );
 }
 
 export default App;
